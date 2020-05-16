778f94c6d75d222edc275aac9ef191e6e85f7267
JK <180682696@stu.vtc.edu.hk>
2020-05-17 04:47:22 +0800

    permission rule fix
    transaction [X] create update delete

--- resources/views/transaction-management/transactions/index.blade.php
+++ resources/views/transaction-management/transactions/index.blade.php
@@ -3,9 +3,9 @@
 @can('transaction_create')
 <div style="margin-bottom: 10px;" class="row">
     <div class="col-lg-12">
-        <a class="btn btn-success" href="{{ route("TransactionManagement.Transactions.create") }}">
+        <!-- <a class="btn btn-success" href="{{ route("TransactionManagement.Transactions.create") }}">
             {{ trans('global.add') }} {{ trans('cruds.transactionManagement.transaction.title') }}
-        </a>
+        </a> -->
     </div>
 </div>
 @endcan
