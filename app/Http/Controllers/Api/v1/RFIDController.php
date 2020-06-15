0a8dca682d595591ccf61ff4af5d2387d3f28e5c
HuanXiang <qq418610675@gmail.com>
2020-06-15 09:11:26 +0800

    update RFID JRD-100

--- app/Http/Controllers/Api/v1/RFIDController.php
+++ app/Http/Controllers/Api/v1/RFIDController.php
@@ -15,19 +15,50 @@ class RFIDController extends ApiController {
                 return parent::sendError('No authentication token issued', 210);
             }
 
-            $rfid = $request->rfid;
-            if($rfid = ShopProductInventory::with('hasShopProduct','hasShopProduct.hasProduct')->where('rfid_code', $rfid)->first()) {
-                if($rfid->is_sold!=1){ //todo Confirm is_sold data dictionary
-                    return parent::sendError('The product cannot be purchased', 211);
+            $data = $request->rfid;
+
+            $rfid = "";
+            $products = [];
+            $productCount = 0;
+            $resultCount = 0;
+            $resultIsSoldCount = 0;
+
+            if(strlen($data) > 16) {
+                if(strlen($data) % 48 == 0) {
+                    $productCount = strlen($data) / 48;
+                    for($i = 0; $i<$productCount;$i++){
+                        $rfid = substr($data,16+($i*48),24);
+                        if($rfid = ShopProductInventory::with('hasShopProduct', 'hasShopProduct.hasProduct')->where('rfid_code', $rfid)->first()) {
+                            if($rfid->is_sold == 1) {
+                                $rfid->hasShopProduct->hasProduct->image = asset($rfid->hasShopProduct->hasProduct->image);
+                                event(new RFID($rfid));   // boardcast to the channel
+                                $resultCount++;
+                                array_push($products,$rfid);
+                            }else{
+                                $resultIsSoldCount++;
+                            }
+                        }
+                    }
+                }else{
+                    return parent::sendError('Data Format Error', 209);
                 }
-
-                // todo update image link
-                $rfid->hasShopProduct->hasProduct->image = asset($rfid->hasShopProduct->hasProduct->image);
-
-                event(new RFID($rfid));   // boardcast to the channel
-                return parent::sendResponse('rfid', $rfid, 'RFID successfully');
+            }else{
+                return parent::sendError('Data Error', 210);
             }
-            return parent::sendError('Not fount product', 212);
+
+            if($resultCount==0){
+                return parent::sendError('Not found product', 211);
+            }elseif($resultCount<$productCount){
+                if($resultIsSoldCount>0){
+                    return parent::sendError('Some product cannot be purchased', 212);
+                }else{
+                    return parent::sendError('Some product Not fount', 213);
+                }
+            }elseif($resultCount==$productCount){
+                return parent::sendResponse('rfid', $products, 'RFID successfully');
+            }
+
+
 
         } catch (\Exception $e) {
             return parent::sendError($e->getMessage(), 215);
