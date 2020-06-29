c5fec0f2bad1f529f479c377f7bf54af7a00a51b
Ken So <180297708@stu.vtc.edu.hk>
2020-06-29 17:01:28 +0800

    ObjectDetection test

--- app/Http/Controllers/Api/v1/ObjectDetectionController.php
+++ app/Http/Controllers/Api/v1/ObjectDetectionController.php
@@ -8,39 +8,39 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
 
-class ObjectDetectionController extends ApiController {
-    public function object_list(Request $request) {
+class ObjectDetectionController extends ApiController
+{
+    public function object_list(Request $request)
+    {
         try {
             $request = $request->json()->all();
             $product_data = $request['product'];
             $product_list = [];
-            foreach ($product_data as $datas){
-                foreach ($datas as $data){
-                    if(array_key_exists($data,$product_list)){
+            foreach ($product_data as $datas) {
+                foreach ($datas as $data) {
+                    if (array_key_exists($data, $product_list)) {
                         $product_list[$data]['amount'] += 1;
-                    }else{
-                        $product_list[$data]=[
-                            'id'=>config('product_object.'.$data),
+                    } else {
+                        $product_list[$data] = [
+                            'id' => $data,
                             'amount' => 1,
-                            'product_detail' => Product::find(config('product_object.'.$data)),
+                            'product_detail' => Product::find($data),
                         ];
                     }
                 }
             }
 
             $product_names = array_keys($product_list);
-            foreach ($product_names as $names){
+            foreach ($product_names as $names) {
                 $product_list[$names]['amount'] /= 10;
             }
 
-            if(count($product_list)!=0){
+            if (count($product_list) != 0) {
                 event(new ObjectDetection($product_list));
             }
             return parent::sendResponse('object', "true", 'Object Detection successfully');
-
         } catch (\Exception $e) {
             return parent::sendError($e->getMessage(), 215);
         }
     }
-
 }
