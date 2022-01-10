71eabc5965d2d936f614cc4a05de372a56b087e3
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-01-10 22:44:07 +0800

    init

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MainController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function login(){
+
+        return '1234';
+    }
+}
