f7b65e7c024510242f4dee872fa38d218044def5
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-01-10 22:53:07 +0800

    add api route

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,21 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/login', 'MainController@login');
+
+$router->post('/user/login', 'MainController@loginUser');
+$router->post('/user/login/device', 'MainController@loginDevice');
+$router->post('/user/logout', 'MainController@logoutUser');
+$router->post('/user', 'MainController@createUser');
+$router->put('/user/{userid}}', 'MainController@updateUser');
+$router->delete('/user/{userid}}', 'MainController@removeUser');
+
+$router->get('/user/{userid}/creditcard', 'MainController@getCreditCard');
+$router->post('/user/{userid}/creditcard', 'MainController@addCreditCard');
+$router->put('/user/{userid}/creditcard', 'MainController@updateCreditCard');
+$router->delete('/user/{userid}/creditcard', 'MainController@removeCreditCard');
+
+$router->get('/user/{userid}/buylists', 'MainController@getBuylists');
+$router->get('/user/{userid}/buylist/{buylistId}', 'MainController@getBuylist');
+$router->post('/user/{userid}/buylist/{buylistId}', 'MainController@addBuylist');
+$router->put('/user/{userid}/buylist/{buylistId}', 'MainController@updateBuylist');
+$router->delete('/user/{userid}/buylist/{buylistId}', 'MainController@removeBuylist');
