6d363936c25d4689e71007a793713311ae797606
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-01-13 01:06:36 +0800

    Create MainController.php

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MainController extends Controller
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function login(){
+
+        return '1234';
+    }
+
+    public function loginDevice(){
+
+        return '1234';
+    }
+}
