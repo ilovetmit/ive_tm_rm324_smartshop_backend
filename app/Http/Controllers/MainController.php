2e9081ca6ac9694ceaecf3d447ce26f2891b941a
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-01-20 14:34:24 +0800

    update api

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,32 +8,17 @@ class MainController extends Controller
     {
         
     }
-
-    public function login(){
-
-        return '1234';
-    }
     public function loginUser(){
-
-        return '{
-            "result": true,
-            "id": 64,
-            "token": "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"
-          }';
+        return response()->json(['result' => true,'id' => 64, 'token' => "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"],200);
     }
     public function loginDevice(){
-
-        return '{
-            "result": true,
-            "token": "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"
-          }';
+        return response() -> json(['result' => true, 'token' => "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"],200);
     }
     public function logoutUser(){
-
-        return '{
-            "code": 200,
-            "type": "result",
-            "message": "Success"
-          }';
+        return response()->json(['code'=>true,'type'=>"result",'message'=>"Success"]);
+    }
+    public function testdb(){
+        $result = app('db')->select('SELECT * FROM product;');
+        return response($result,200);
     }
 }
