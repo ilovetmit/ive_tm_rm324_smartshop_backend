8c0bae274b2a8677adc7325537f723d89a23347c
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-01-21 20:32:07 +0800

    Request $request

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,22 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Client\Request;
+
 class MainController extends Controller
 {
     public function __construct()
     {
         
     }
-    public function loginUser(){
+    public function loginUser(Request $request){
         return response()->json(['result' => true,'id' => 64, 'token' => "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"],200);
     }
-    public function loginDevice(){
+    public function loginDevice(Request $request){
         return response() -> json(['result' => true, 'token' => "Tzq88tcwx5QWkKnjnLHks2C6evPL2wwLPbkHYrMDbDuNngJhkpaWEHCS4CcsqCsp"],200);
     }
-    public function logoutUser(){
+    public function logoutUser(Request $request){
         return response()->json(['code'=>true,'type'=>"result",'message'=>"Success"]);
     }
-    public function testdb(){
+    public function testdb(Request $request){
         $result = app('db')->select('SELECT * FROM product;');
         return response($result,200);
     }
