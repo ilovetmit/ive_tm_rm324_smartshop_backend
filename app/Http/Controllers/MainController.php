294a7b6547a68ed6758cd82cdceea7fab74d08e3
Jack Man <29057227+jackman815@users.noreply.github.com>
2022-02-05 12:32:02 +0800

    reformat

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -84,10 +84,10 @@ class MainController extends Controller
             'name' => $name,
             'tel' => $tel
         ]);
-        if ($id>0) {
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
+        if ($id > 0) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
         } else {
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
     public function updateUser(Request $request)
@@ -99,203 +99,212 @@ class MainController extends Controller
         $name = $request->name;
         $tel = $request->tel;
         $updatePassword = $request->input('updatePassword');
-        if ($updatePassword){
+        if ($updatePassword) {
             $affected = DB::table('user')
-            ->where('userid', '=', $userid)
-            ->update([
-                'email' => $email,
-                'password' => $password,
-                'name' => $name,
-                'tel' => $tel
-            ]);
-        }else{
-            $affected = DB::table('user')
-            ->where('userid', '=', $userid)
-            ->update([
-                'email' => $email,
-                'name' => $name,
-                'tel' => $tel
-            ]);
-        }
-        
-        if ($affected > 0) {
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
+                ->where('userid', '=', $userid)
+                ->update([
+                    'email' => $email,
+                    'password' => $password,
+                    'name' => $name,
+                    'tel' => $tel
+                ]);
         } else {
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+            $affected = DB::table('user')
+                ->where('userid', '=', $userid)
+                ->update([
+                    'email' => $email,
+                    'name' => $name,
+                    'tel' => $tel
+                ]);
+        }
+
+        if ($affected > 0) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
+        } else {
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
-    public function removeUser(Request $request){
+    public function removeUser(Request $request)
+    {
         $userid = $request->userid;
         $affected = DB::table('user')
-            ->where('userid','=',$userid)
+            ->where('userid', '=', $userid)
             ->delete();
-            if ($affected > 0) {
-                return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
-            } else {
-                return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
-            }
+        if ($affected > 0) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
+        } else {
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
+        }
     }
-    public function getBuylists(Request $request,$userid){
-        $result = DB::table('buylist')->where('userid','=',$userid)->get();
+    public function getBuylists(Request $request, $userid)
+    {
+        $result = DB::table('buylist')->where('userid', '=', $userid)->get();
         return response()->json(['result' => $result]);
     }
-    public function getBuylist(Request $request, $userid,$buylistId){
-        $result = DB::table('buylistdetails')->where('buyid','=',$buylistId)->get();
+    public function getBuylist(Request $request, $userid, $buylistId)
+    {
+        $result = DB::table('buylistdetails')->where('buyid', '=', $buylistId)->get();
         $items = [];
-        foreach($result as $row){
-            $items.array_push([$result->productid,$result->qty]);
+        foreach ($result as $row) {
+            $items . array_push([$result->productid, $result->qty]);
         }
-        return response()->json(['id' => $buylistId, 'item' => $items],200);
+        return response()->json(['id' => $buylistId, 'item' => $items], 200);
     }
-    public function addBuylist(Request $request, $userid){
+    public function addBuylist(Request $request, $userid)
+    {
         $name = $request->name;
         //return print_r($request->items);
         $items = $request->items;
         //return count($items);
-        if (count($items) > 0){
+        if (count($items) > 0) {
             $buylistId = DB::table('buylist')->insertGetId([
                 'name' => $name,
                 'userid' => $userid
             ]);
-            if($buylistId>0){
-                foreach($items as $item){
+            if ($buylistId > 0) {
+                foreach ($items as $item) {
                     //$product = DB::table('product')->where('name','=', $name)->get();
-                        $result = DB::table('buylistdetails')->insert([
-                            'buyid' => $buylistId,
-                            'productid' => $item['productid'],
-                            'qty' => $item['quantity']
-                        ]);
-                        if($result){
-                            continue;
-                        }else{
-                            return response()->json(['code' => 400, 'type' => "error", 'message' => "Product Lookup Error"],400);
-                            break;
-                        }
+                    $result = DB::table('buylistdetails')->insert([
+                        'buyid' => $buylistId,
+                        'productid' => $item['productid'],
+                        'qty' => $item['quantity']
+                    ]);
+                    if ($result) {
+                        continue;
+                    } else {
+                        return response()->json(['code' => 400, 'type' => "error", 'message' => "Product Lookup Error"], 400);
+                        break;
+                    }
                 }
-            }else{
-                return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert buylist error"],400);
+            } else {
+                return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert buylist error"], 400);
             }
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
         }
     }
-    public function updateBuylist(Request $request,$userid){
+    public function updateBuylist(Request $request, $userid)
+    {
         $buylistId = $request->id;
         $name = $request->name;
         $items = $request->items;
 
-        if (count($items) > 0){
-            $results = DB::table('buylist')->where('buyid','=',$buylistId)->update([
+        if (count($items) > 0) {
+            $results = DB::table('buylist')->where('buyid', '=', $buylistId)->update([
                 'name' => $name
             ]);
-            if($results){
-                $affected_item = DB::table('buylistdetails')->where('buyid','=',$buylistId)->delete();
-                foreach($items as $item){
-                    $product = DB::table('product')->where('name','=', $request->name)->first();
-                    if (count($product)>0){
+            if ($results) {
+                $affected_item = DB::table('buylistdetails')->where('buyid', '=', $buylistId)->delete();
+                foreach ($items as $item) {
+                    $product = DB::table('product')->where('name', '=', $request->name)->first();
+                    if (count($product) > 0) {
                         $result = DB::table('buylistdetails')->insert([
                             'buyid' => $buylistId,
                             'productid' => $item->productid,
                             'qty' => $item->quantity
                         ]);
-                        if($result){
+                        if ($result) {
                             continue;
-                        }else{
-                            return response()->json(['code' => 400, 'type' => "error", 'message' => "Product Lookup Error"],400);
+                        } else {
+                            return response()->json(['code' => 400, 'type' => "error", 'message' => "Product Lookup Error"], 400);
                             break;
                         }
-                    }else{
-                        return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert item error"],400);
+                    } else {
+                        return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert item error"], 400);
                         break;
                     }
                 }
-            }else{
-                return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert buylist error"],400);
+            } else {
+                return response()->json(['code' => 400, 'type' => "error", 'message' => "Insert buylist error"], 400);
             }
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
         }
-
     }
-    public function removeBuylist(Request $request, $userid){
+    public function removeBuylist(Request $request, $userid)
+    {
         $buyid = $request->buylistId;
-        $affected_item = DB::table('buylistdetails')->where('buyid','=',$buyid)->delete();
-        $affected = DB::table('buylist')->where('buyid','=',$buyid)->delete();
+        $affected_item = DB::table('buylistdetails')->where('buyid', '=', $buyid)->delete();
+        $affected = DB::table('buylist')->where('buyid', '=', $buyid)->delete();
         if ($affected > 0 && $affected_item > 0) {
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
         } else {
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
-    public function getProduct(Request $request){
+    public function getProduct(Request $request)
+    {
         $method = intval($request->input('method'));
-        if(!$method){
+        if (!$method) {
             $method = 1;
         }
-        switch($method){
-            case(1): //GetProductByid
+        switch ($method) {
+            case (1): //GetProductByid
                 $productid = $request->input('productid');
-                $result = DB::table('product')->where('productid','=',$productid)->first();
-                if ($result){
-                    return response()->json(["id" => $result->productid, "name" => $result->name, "description"=>$result->description, "price"=>$result->price, "Location"=>$result->Location],200);
-                }else{
-                    return response()->json(["id" => "", "name" => "", "description"=>"", "price"=>"", "Location"=>""],400);
+                $result = DB::table('product')->where('productid', '=', $productid)->first();
+                if ($result) {
+                    return response()->json(["id" => $result->productid, "name" => $result->name, "description" => $result->description, "price" => $result->price, "Location" => $result->Location], 200);
+                } else {
+                    return response()->json(["id" => "", "name" => "", "description" => "", "price" => "", "Location" => ""], 400);
                 }
                 break;
-            case(2)://GetProductByName
+            case (2): //GetProductByName
                 $name = $request->input('name');
-                $result = DB::table('product')->where('name','=',$name)->first();
-                if ($result){
-                    return response()->json(["id" => $result->productid, "name" => $result->name, "description"=>$result->description, "price"=>$result->price, "Location"=>$result->Location],200);
-                }else{
-                    return response()->json(["id" => "", "name" => "", "description"=>"", "price"=>"", "Location"=>""],400);
+                $result = DB::table('product')->where('name', '=', $name)->first();
+                if ($result) {
+                    return response()->json(["id" => $result->productid, "name" => $result->name, "description" => $result->description, "price" => $result->price, "Location" => $result->Location], 200);
+                } else {
+                    return response()->json(["id" => "", "name" => "", "description" => "", "price" => "", "Location" => ""], 400);
                 }
                 break;
-            case(3)://GetProductByKeywords
+            case (3): //GetProductByKeywords
                 $name = $request->input('name');
-                $result = DB::table('product')->where('name','LIKE','%'.$name.'%')->get();
-                if ($result->count()>0){
-                    return response()->json($result,200);
-                }else{
-                    return response()->json(["id" => "", "name" => "", "description"=>"", "price"=>"", "Location"=>""],400);
+                $result = DB::table('product')->where('name', 'LIKE', '%' . $name . '%')->get();
+                if ($result->count() > 0) {
+                    return response()->json($result, 200);
+                } else {
+                    return response()->json(["id" => "", "name" => "", "description" => "", "price" => "", "Location" => ""], 400);
                 }
                 break;
         }
     }
-    public function addProduct(Request $request){
+    public function addProduct(Request $request)
+    {
         $result = DB::table('product')->insertGetId([
-            'name'=>$request->name,
-            'description'=>$request->description,
-            'price'=>$request->price,
-            'Location'=>$request->Location
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'Location' => $request->Location
         ]);
-        if($result){
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
-        }else{
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+        if ($result) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
+        } else {
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
-    public function updateProduct(Request $request){
+    public function updateProduct(Request $request)
+    {
         $productid = $request->productid;
         $affected = DB::table('product')
-            ->where('productid','=',$productid)
+            ->where('productid', '=', $productid)
             ->update([
-            'name'=>$request->name,
-            'description'=>$request->description,
-            'price'=>$request->price,
-            'Location'=>$request->Location
-        ]);
-        if ($affected){
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
-        }else{
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+                'name' => $request->name,
+                'description' => $request->description,
+                'price' => $request->price,
+                'Location' => $request->Location
+            ]);
+        if ($affected) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
+        } else {
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
-    public function removeProduct(Request $request){
+    public function removeProduct(Request $request)
+    {
         $productid = $request->productid;
-        $affected = DB::table('product')->where('productid','=',$productid)->delete();
-        if ($affected){
-            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"],200);
-        }else{
-            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"],400);
+        $affected = DB::table('product')->where('productid', '=', $productid)->delete();
+        if ($affected) {
+            return response()->json(['code' => 200, 'type' => "result", 'message' => "Success"], 200);
+        } else {
+            return response()->json(['code' => 400, 'type' => "error", 'message' => "General Error"], 400);
         }
     }
 
